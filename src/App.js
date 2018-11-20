7dfb597d8c8e15a3dc0219a289549440213655e4
Justin Lee <leejwg.3@gmail.com>
2018-11-20 20:52:07 +0900

    adding some CRUD

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
 
+// small standar DC that renders the hooklist props
+const HookList = ({ hookList }) => (
+  <div className="hook-list">{hookList.mumble}</div>
+);
+// initialization of app
 function App() {
-  const [hookList] = useState([
-    { text: "hooks" },
-    { text: "fishing" },
-    { text: "fish for fishing" }
+  const [hookList, hookListItems] = useState([
+    { mumble: "hooks, also know as left hook, right hook" },
+    { mumble: "fishing, or is it phising?? or fssssshing???" },
+    { mumble: "can you make sense of the mumbles in this rumble?" },
+    { mumble: "mumbling again, mumble Mc Mo Mumble" }
   ]);
 
-  const HookList = ({ hookList }) => (
-    <div className="hook-list">{hookList.text}</div>
-  );
-
-  // Starting some hooks or so i think
+  // Standard map render
   return (
     <div className="app">
       <div className="hook-list">
